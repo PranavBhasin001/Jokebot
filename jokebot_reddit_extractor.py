202dc22d26094bf74bcae1ab4d34c824ec945008
Pranav Bhasin <pranav.bhasin@berkeley.edu>
2019-01-09 13:00:29 +0530

    Update jokebot_reddit_extractor.py
    Added documentation for the file

--- jokebot_reddit_extractor.py
+++ jokebot_reddit_extractor.py
@@ -10,13 +10,14 @@ import requests
 
 
 def request_page_json(reddit_url):
-    """"""
+    """Requests page data in JSON format from the provided url"""
     r = requests.get(reddit_url, headers={'User-agent': 'jokebot_2.0'})
     return r.json()
 
 
 def json_to_list(json_object):
-    """"""
+    """Converts the provided JSON data into a list of lists containing prompt-punchline jokes
+    that are safe for work and are interrogative in nature"""
     json_data = json_object['data']['children']
     joke_list = list()
 
@@ -29,20 +30,21 @@ def json_to_list(json_object):
 
 
 def check_safe_for_work(joke_data):
+    """Returns True if the joke data's over_18 parameter is False, else returns False"""
     return False if joke_data["data"]["over_18"] else True
 
 
 def check_interrogative(joke_data):
-    """"""
+    """Returns True if the joke's punchline starts with How, What or Why, else returns False"""
     condition = get_prompt(joke_data).split(" ")[0] in ["How", "What", "Why"]
     return True if condition else False
 
 
 def get_prompt(joke_data):
-    """"""
+    """Returns the prompt from joke_data"""
     return joke_data["data"]["title"]
 
 
 def get_punchline(joke_data):
-    """"""
+    """Returns the punchline associated with joke_data"""
     return joke_data["data"]["selftext"]
